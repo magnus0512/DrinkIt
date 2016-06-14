dce96483bc4865790ca27d9687124ff428fc66c9
Frederik <Frederik.franklin.eriksen@gmail.com>
2016-06-14 15:29:26 +0200

    Afstandsberegner sat ind i MainActivity

--- app/src/main/java/gruppe7/drinkit/Locations.java
+++ app/src/main/java/gruppe7/drinkit/Locations.java
@@ -12,6 +12,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.location.Location;
 import android.location.LocationManager;
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -31,13 +32,42 @@ import android.widget.RelativeLayout;
 public class Locations extends Activity {
 
     LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    String locationProvider = LocationManager.NETWORK_PROVIDER;
+
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
     }
+
+
+    public double afstandsberegner(Location newLocation) {
+        try {
+            Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+            double R = 6371000;
+            double lat1 = lastKnownLocation.getLatitude();
+            double lat2 = newLocation.getLatitude();
+            double long1 = lastKnownLocation.getLongitude();
+            double long2 = newLocation.getLongitude();
+
+            double deltaLat = Math.abs(lat2-lat1)*Math.PI/180;
+            double deltaLong = Math.abs(long2-long1)*Math.PI/180;
+            double a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
+                        Math.cos(lat1) * Math.cos(lat2) *
+                        Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            return (R * c);
+
+            // Se http://www.movable-type.co.uk/scripts/latlong.html for formler
+        } catch (SecurityException e){
+            return 0;
+        }
+    }
+
 
 }
 
