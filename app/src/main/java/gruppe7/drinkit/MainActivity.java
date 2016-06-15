373535f783bcd81cc3ea6404fe46e7b004551f02
namanhnguyen <s154230@student.dtu.dk>
2016-06-15 13:40:10 +0200

    Rejected commit: Default

--- app/src/main/java/gruppe7/drinkit/MainActivity.java
+++ app/src/main/java/gruppe7/drinkit/MainActivity.java
@@ -1,40 +1,115 @@
-package gruppe7.drinkit;
+package swprojekt.drinkit.testingtabs;
 
+import android.app.FragmentTransaction;
 import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
 import android.location.LocationManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
+    final private static String APP_TITLE = "DTU DrinkIt";
 
     LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
     String locationProvider = LocationManager.NETWORK_PROVIDER;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(APP_TITLE);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Coffee"));
+        tabLayout.addTab(tabLayout.newTab().setText("Beer"));
+
+        // Overvej lige hvad den her gør ...
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
     }
+
+
+    // Husk at ændre til ContextMenu hvis settings skal være sådan i stedet
+
+    // Opretter layout for toolbar
     @Override
-    protected void onResume() {
-        super.onResume();
-
-
-
-
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
     }
 
+    // Åbner settings menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
 
+            // TODO: Åben settings menu
 
+            return true;
+        }
 
+        return super.onOptionsItemSelected(item);
+    }
+/*
+    // Called when the user selects an item in the TabFragment
+    @Override
+    public void onListSelection(int index) {
 
+        // If the QuoteFragment has not been added, add it now
+        if (!mDetailsFragment.isAdded()) {
 
+            // Start a new FragmentTransaction
+            FragmentTransaction fragmentTransaction = mFragmentManager
+                    .beginTransaction();
 
+            // Add the QuoteFragment to the layout
+            fragmentTransaction.add(R.id.quote_fragment_container, mDetailsFragment);
 
+            // Add this FragmentTransaction to the backstack
+            fragmentTransaction.addToBackStack(null);
 
+            // Commit the FragmentTransaction
+            fragmentTransaction.commit();
+
+            // Force Android to execute the committed FragmentTransaction
+            mFragmentManager.executePendingTransactions();
+        }
+
+        if (mDetailsFragment.getShownIndex() != index) {
+
+            // TODO: Vis dialogboks
+
+            // Tell the QuoteFragment to show the quote string at position index
+            mDetailsFragment.showQuoteAtIndex(index);
+
+        }
+    }
+    */
 }
