d9f239baed16c4eb494c1e657d483f293f524deb
asmundottesen <asmund.ottesen@gmail.com>
2016-06-16 14:03:55 +0200

    Dialog fragment tilføjet

--- app/src/main/java/gruppe7/drinkit/ListItemFragment.java
+++ app/src/main/java/gruppe7/drinkit/ListItemFragment.java
@@ -1,7 +1,13 @@
 package gruppe7.drinkit;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ShareCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,7 +33,7 @@ public class ListItemFragment extends Fragment {
         barNameButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                AlertDialogFragment.newInstance();
             }
         });
 
@@ -49,6 +55,46 @@ public class ListItemFragment extends Fragment {
 
     public void setBarName(String name) {
         barNameButton.setText(name);
+
+    }
+
+
+    public static class AlertDialogFragment extends DialogFragment {
+
+        public static AlertDialogFragment newInstance() {
+            return new AlertDialogFragment();
+        }
+
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    //Sætter titlen
+                    //Todo: hent titel
+                    .setTitle("Diagonalen")
+                    //Sætter info om bar
+                    //Todo hent info
+                    .setMessage("Åbningstider: Priser: Lokation: ")
+                    //gør det muligt at trykke tilbage
+                    .setCancelable(true)
+                    //Sætter "cancel"-kanppen; lukker dialogboksen
+                    .setNegativeButton(android.R.string.cancel, null)
+                    //Sætter "share"-knappen
+                    //Todo: tilføj share funktion
+/*
+                    .setNeutralButton("Share",
+                            new DialogInterface.OnClickListener() {})
+*/
+                    //Sætter "find"-knappen, *mangler funktion til Maps*
+                    //Todo: Åben Maps med rute
+
+                    .setPositiveButton("Find",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(
+                                        final DialogInterface dialog, int id) {
+
+                                }
+                            })
+                    .create();
+        }
 
     }
 
