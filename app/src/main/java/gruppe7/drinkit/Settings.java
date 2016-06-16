7b64b84a343b4201472c86e570affb9d10448c2d
magnus0512 <magnusdal10@hotmail.com>
2016-06-16 11:51:18 +0200

    Settings Save Options

--- app/src/main/java/gruppe7/drinkit/Settings.java
+++ app/src/main/java/gruppe7/drinkit/Settings.java
@@ -4,9 +4,12 @@ package gruppe7.drinkit;
  * Created by Magnus on 14-06-2016.
  */
 import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -19,16 +22,19 @@ public class Settings extends Activity {
     private CheckBox OpenOnlyCheckBox;
     private Button OkButton, CancelButton;
     private RadioGroup SortRadioGroup, SearchRadioGroup;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
         addListenerOkButton();
         addListenerCancelButton();
+
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(getApplicationContext());
+        OpenOnlyCheckBox.setChecked(sharedPreferences.getBoolean("CheckButton", false));
+        SearchRadioGroup.check(sharedPreferences.getInt("SearchRadio", R.id.BeerRadioButton));
+        SortRadioGroup.check(sharedPreferences.getInt("SortRadio", R.id.DistanceRadiobutton));
     }
-
-
     public void addListenerOkButton() {
         OkButton = (Button) findViewById(R.id.OkButton);
         OpenOnlyCheckBox = (CheckBox) findViewById(R.id.checkbox);
@@ -44,9 +50,9 @@ public class Settings extends Activity {
                 int searchId = SearchRadioGroup.getCheckedRadioButtonId();
                 int sortId = SortRadioGroup.getCheckedRadioButtonId();
                 if (OpenOnlyCheckBox.isChecked()) {
-                    result.append("\nOnly Open :").append("Current");
+                    result.append("Only Open :").append("On");
                 }else{
-                    result.append("\nOnly Open :").append("Off");
+                    result.append("Only Open :").append("Off");
                 }
                 if (sortId == R.id.DistanceRadiobutton) {
                     result.append("\nDistance :").append("Current");
@@ -63,6 +69,15 @@ public class Settings extends Activity {
                 }
                 Toast.makeText(Settings.this, result.toString(),
                         Toast.LENGTH_LONG).show();
+
+                SharedPreferences sharedPreferences = PreferenceManager
+                        .getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean("CheckButton", OpenOnlyCheckBox.isChecked());
+                editor.putInt("SearchRadio", SearchRadioGroup.getCheckedRadioButtonId());
+                editor.putInt("SortRadio", SortRadioGroup.getCheckedRadioButtonId());
+                editor.commit();
+                finish();
             }
         });
     }
@@ -76,7 +91,6 @@ public class Settings extends Activity {
             }
         });
     }
-
     @Override
     protected void onResume() {
         super.onResume();
